df100ffef559129b989e4238fc027f1fa04fa654
Covenant-Tandu11 <covenanttandu9@gmail.com>
2026-05-03 17:38:19 +0100

    Worked on the hamburger functionality

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-// ==============
+// ============================================
+// HAMBURGER MENU TOGGLE
+// ============================================
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    if(hamburger && mobileMenu){
+        hamburger.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            mobileMenu.classList.toggle('mobile-menu-active');
+            hamburger.classList.toggle('active');
+
+            if(mobileMenu.classList.contains('mobile-menu-active')){
+                hamburger.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+            }else{
+                hamburger.innerHTML = '<i class="fa-solid fa-bars"></i>'
+            }
+        });
+
+        const menuLinks = mobileMenu.querySelectorAll('a');
+        menuLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                mobileMenu.classList.remove('mobile-menu-active');
+                hamburger.classList.remove('active');
+                hamburger.innerHTML = '<i class="fa-solid fa-bars"></i>';
+            });
+        });
+    };
+})
